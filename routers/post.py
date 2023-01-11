2c8169f1a2197e64f48d8a9b9fde20ecd6d41c04
madvirus-ops <edwinayabie1@gmail.com>
2023-01-11 12:46:00 +0100

    cretae post

--- routers/post.py
+++ routers/post.py
@@ -27,6 +27,15 @@ async def get_post_by_id(id:int,db:Session=Depends(get_db)):
 
 
 
+@router.post("/",status_code=status.HTTP_201_CREATED)
+async def create_post(post:schemas.Posts,db:Session=Depends(get_db)):
+    """create a new blogpost"""
+    new_post = models.BlogModel(title=post.title,body=post.content)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
 
 
     
