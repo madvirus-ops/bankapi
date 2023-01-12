5a69f52ae580a8bf960ef8dabbd0bdb214e162d3
madvirus-ops <edwinayabie1@gmail.com>
2023-01-12 14:23:16 +0100

    hashed password

--- helpers.py
+++ helpers.py
@@ -1,7 +1,9 @@
 from fastapi import status,HTTPException
+from passlib.context import CryptContext
 
 
 
+pwd_context = CryptContext(schemes=['bcrypt'],deprecated="auto")
 
 
 def get_object_or_404(model,id,db):
@@ -11,5 +13,10 @@ def get_object_or_404(model,id,db):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"object with id {id} is not foumd")
 
 
-def hash_password():
+def hash_password(password):
+    hashed_password = pwd_context.hash(password)
+    return hashed_password
+
+def verify_password(hashed_password,password):
+    # pwd = pwd_context.verify()
     pass
--- routers/users.py
+++ routers/users.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,status,Depends,HTTPException
 from database import get_db
 import schemas
 import models
+from helpers import hash_password
 from sqlalchemy.orm import Session 
 
 
@@ -10,7 +11,10 @@ router = APIRouter(prefix="/api/v1/user", tags=['users'])
 
 @router.post("/signup",status_code=status.HTTP_201_CREATED)
 async def create_user(request:schemas.User,db:Session=Depends(get_db)):
-    new_user = models.UserModel(email=request.email,name=request.email,password=request.password1)
+    if request.password1 != request.password2:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="passwords do not match")
+    cleaned_password = hash_password(request.password1)
+    new_user = models.UserModel(email=request.email,name=request.email,password=cleaned_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
