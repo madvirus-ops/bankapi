505a5538d90d3fa2a163979f9633cf6940528a25
madvirus-ops <edwinayabie1@gmail.com>
2023-01-13 12:16:26 +0100

    worked on helpers

--- helpers.py
+++ helpers.py
@@ -1,22 +1,32 @@
 from fastapi import status,HTTPException
 from passlib.context import CryptContext
+import models
+from sqlalchemy.orm import Session
 
 
 
 pwd_context = CryptContext(schemes=['bcrypt'],deprecated="auto")
 
 
-def get_object_or_404(model,id,db):
+def get_object_or_404(model,id,db:Session):
     object = db.query(model).filter(model.id == id).first()
     if object:
         return object
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"object with id {id} is not foumd")
 
 
+
 def hash_password(password):
     hashed_password = pwd_context.hash(password)
     return hashed_password
 
+
 def verify_password(hashed_password,password):
     return pwd_context.verify(hashed_password,password)
+
+
+
+def get_user_by_email(email,db:Session):
+    user = db.query(models.UserModel).filter(models.UserModel.email == email).first()
+    return user
    
--- routers/users.py
+++ routers/users.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter,status,Depends,HTTPException
 from database import get_db
 import schemas
 import models
-from helpers import hash_password
+from helpers import get_user_by_email,get_object_or_404
+from crud import UserCrud
 from sqlalchemy.orm import Session 
 
 
@@ -11,19 +12,14 @@ router = APIRouter(prefix="/api/v1/user", tags=['users'])
 
 @router.post("/signup",status_code=status.HTTP_201_CREATED)
 async def create_user(request:schemas.User,db:Session=Depends(get_db)):
-    if request.password1 != request.password2:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="passwords do not match")
-    if len(request.password1) < 6 :
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="make password 8 characters")
-    cleaned_password = hash_password(request.password1)
-    new_user = models.UserModel(email=request.email,name=request.email,password=cleaned_password)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    verify = get_user_by_email(request.email,db)
+    if verify:
+        raise HTTPException(status_code=status.HTTP_207_MULTI_STATUS,detail="user with email exists")
+    new_user = UserCrud.create_user(request,db)
     return new_user
     
 
 @router.get("/{id}",response_model=schemas.ShowUser)
 async def get_user_and_posts(id:int, db:Session = Depends(get_db)):
-    user = db.query(models.UserModel).filter(models.UserModel.id == id).first()
+    user = get_object_or_404(models.UserModel,id,db)
     return user
