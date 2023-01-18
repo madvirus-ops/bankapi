fbf40e2d2f5c08691d2787752005dde9158f72d0
madvirus-ops <edwinayabie1@gmail.com>
2023-01-18 11:23:18 +0100

    working on webhookf

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from routers import post,users,authentication,banking,webhook
+import models
+from database import engine
+
+app = FastAPI(title="Banke App",description="mimicking the backend of basic fintech app")
+
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(authentication.router)
+app.include_router(post.router)
+app.include_router(users.router)
+app.include_router(banking.router)
+app.include_router(webhook.router)
+
+@app.get("/")
+async def home():
+    return {"home":"homepage on"}
