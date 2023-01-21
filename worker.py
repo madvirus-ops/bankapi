a3e614a417bd0839fc5ae5f37f36b45dc323f769
madvirus-ops <edwinayabie1@gmail.com>
2023-01-21 18:31:56 +0100

    working on email alerts

--- worker.py
+++ worker.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import Session
 import json
 from fastapi import Depends,status,HTTPException
 import models
+from datetime import datetime,timedelta,timezone
+from jose import jwt,JWTError
+from helpers import get_user_by_id
 
+
+SECRET_KEY='secret'
+ALGORITHM='HS256'
 
 def transfer_to_wallet(db:Session,toUser,User,Amount,pin):
     
@@ -37,3 +43,25 @@ def transfer_to_wallet(db:Session,toUser,User,Amount,pin):
         "pin":from_pinn.pin,
         "sent":pin
     }
+
+
+def verification_code(user_id):
+    data={'sub':user_id, 'type':'verify_email_code', 'exp':datetime.now(tz=timezone.utc)+timedelta(minutes=5)}
+    encoded=jwt.encode(data,SECRET_KEY, algorithm=ALGORITHM )
+    return encoded
+
+def verify_email_code(token, db:Session):
+    exception= HTTPException(status_code=400,  detail='invalid token or token has expired')
+    try:
+        payload=jwt.decode(token, SECRET_KEY)
+        user=get_user_by_id(db, payload.get('sub'))
+        if payload.get('type') != 'verify_email_code':
+            raise exception
+        elif not user:
+            raise exception       
+    except Exception as e:
+        raise exception
+    user.email_verified=True
+    db.commit()
+    db.refresh(user)
+    return True
