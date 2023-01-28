40676bab088c1cb882e5c8c20d7cd2a7a0f0e215
madvirus-ops <edwinayabie1@gmail.com>
2023-01-28 15:11:45 +0100

    cyberdata as provider

--- routers/virtual_tp.py
+++ routers/virtual_tp.py
@@ -4,3 +4,13 @@ import models
 import schemas
 from worker import env_config
 from fastapi_mail import MessageSchema,FastMail
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+router = APIRouter(prefix="/api/v1/vtu",tags=['virtual top up'])
+cyb_key = os.getenv("CYBERDATA_KEY")
+
+
+    
