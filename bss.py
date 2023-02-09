f62a71ce516ee36893a577974232268955aa47e4
madvirus-ops <edwinayabie1@gmail.com>
2023-02-09 16:08:41 +0100

    extract cyberdata plans to json

---
+++ bss.py
@@ -0,0 +1,816 @@
+#this is the extracted cyberdata price list for data,network and cable
+
+
+plans = [
+    {
+        "data_id": "7",
+        "Network": "MTN",
+        "plan_type": "SME",
+        "Amount": "₦230",
+        "Size": "1.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "8",
+        "Network": "MTN",
+        "plan_type": "SME",
+        "Amount": "₦460",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "11",
+        "Network": "MTN",
+        "plan_type": "SME",
+        "Amount": "₦1150",
+        "Size": "5.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "43",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦3360",
+        "Size": "12.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "44",
+        "Network": "MTN",
+        "plan_type": "SME",
+        "Amount": "₦690",
+        "Size": "3.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "50",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦4800",
+        "Size": "20.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "51",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦9600",
+        "Size": "40.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "52",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦14440",
+        "Size": "75.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "145",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦940",
+        "Size": "1.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "146",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦1128",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "147",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦1410",
+        "Size": "3.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "148",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦1880",
+        "Size": "4.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "149",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦2350",
+        "Size": "6.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "150",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦2820",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "209",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦480",
+        "Size": "750.0 MB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "164",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦4700",
+        "Size": "20.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "165",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦9400",
+        "Size": "40.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "182",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦455",
+        "Size": "500.0 MB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "183",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦900",
+        "Size": "1.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "184",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1080",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "185",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1350",
+        "Size": "3.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "186",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1800",
+        "Size": "4.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "187",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦3600",
+        "Size": "11.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "188",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦4500",
+        "Size": "15.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "189",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦9000",
+        "Size": "40.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "190",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦13500",
+        "Size": "75.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "191",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦14100",
+        "Size": "75.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "240",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦1440",
+        "Size": "6.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "193",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦18800",
+        "Size": "110.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "194",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦470",
+        "Size": "1.35 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "195",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦940",
+        "Size": "2.9 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "196",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦1410",
+        "Size": "4.1 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "197",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦1880",
+        "Size": "5.8 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "198",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦2350",
+        "Size": "7.7 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "199",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦2820",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "200",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦3760",
+        "Size": "13.25 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "201",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦4700",
+        "Size": "18.25 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "202",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦7520",
+        "Size": "29.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "203",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦9400",
+        "Size": "50.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "204",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦14100",
+        "Size": "93.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "205",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦16920",
+        "Size": "119.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "206",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦18800",
+        "Size": "138.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "213",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦1920",
+        "Size": "4.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "214",
+        "Network": "GLO",
+        "plan_type": "GIFTING",
+        "Amount": "₦1410",
+        "Size": "7.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "215",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦19200",
+        "Size": "120.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "220",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1000",
+        "Size": "1.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "221",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1200",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "222",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1500",
+        "Size": "3.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "223",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1500",
+        "Size": "6.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "224",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦2000",
+        "Size": "5.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "225",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦3500",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "226",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦10000",
+        "Size": "40.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "227",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦5000",
+        "Size": "15.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "228",
+        "Network": "SMILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦10000",
+        "Size": "100.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "244",
+        "Network": "MTN",
+        "plan_type": "SME",
+        "Amount": "₦135",
+        "Size": "500.0 MB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "236",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦2880",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "243",
+        "Network": "MTN",
+        "plan_type": "CORPORATE",
+        "Amount": "₦150",
+        "Size": "500.0 MB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "229",
+        "Network": "MTN",
+        "plan_type": "CORPORATE",
+        "Amount": "₦240",
+        "Size": "1.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "230",
+        "Network": "MTN",
+        "plan_type": "CORPORATE",
+        "Amount": "₦480",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "231",
+        "Network": "MTN",
+        "plan_type": "CORPORATE",
+        "Amount": "₦720",
+        "Size": "3.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "232",
+        "Network": "MTN",
+        "plan_type": "CORPORATE",
+        "Amount": "₦1200",
+        "Size": "5.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "237",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦960",
+        "Size": "1.5 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "238",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦1152",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "239",
+        "Network": "MTN",
+        "plan_type": "GIFTING",
+        "Amount": "₦1440",
+        "Size": "3.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "245",
+        "Network": "MTN",
+        "plan_type": "CORPORATE",
+        "Amount": "₦2400",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "246",
+        "Network": "MTN",
+        "plan_type": "SME",
+        "Amount": "₦2300",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "247",
+        "Network": "9MOBILE",
+        "plan_type": "GIFTING",
+        "Amount": "₦1350",
+        "Size": "7.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "248",
+        "Network": "AIRTEL",
+        "plan_type": "GIFTING",
+        "Amount": "₦1410",
+        "Size": "6.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "262",
+        "Network": "AIRTEL",
+        "plan_type": "CORPORATE",
+        "Amount": "₦135",
+        "Size": "500.0 MB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "263",
+        "Network": "AIRTEL",
+        "plan_type": "CORPORATE",
+        "Amount": "₦225",
+        "Size": "1.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "264",
+        "Network": "AIRTEL",
+        "plan_type": "CORPORATE",
+        "Amount": "₦450",
+        "Size": "2.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "268",
+        "Network": "AIRTEL",
+        "plan_type": "CORPORATE",
+        "Amount": "₦1125",
+        "Size": "5.0 GB",
+        "Validity": "1 month"
+    },
+    {
+        "data_id": "269",
+        "Network": "AIRTEL",
+        "plan_type": "CORPORATE",
+        "Amount": "₦2250",
+        "Size": "10.0 GB",
+        "Validity": "1 month"
+    }
+]
+for key in plans:
+    print(key['data_id'])
+    print(key['Size'])
+    print(key['plan_type'])
+    print(key['Amount'])
+    print("="*20)
+
+network = [
+    {
+        "network_id": "1",
+        "network_name": "MTN"
+    },
+    {
+        "network_id": "2",
+        "network_name": "GLO"
+    },
+    {
+        "network_id": "3",
+        "network_name": "9MOBILE"
+    },
+    {
+        "network_id": "4",
+        "network_name": "AIRTEL"
+    },
+    {
+        "network_id": "5",
+        "network_name": "SMILE"
+    }
+]
+
+cable = [
+    {
+        "cableplan_id": "2",
+        "cableplan_name": "GOtv Max",
+        "cableplan_amount": "4150"
+    },
+    {
+        "cableplan_id": "6",
+        "cableplan_name": "DStv Yanga",
+        "cableplan_amount": "2950"
+    },
+    {
+        "cableplan_id": "7",
+        "cableplan_name": "DStv Compact",
+        "cableplan_amount": "9000"
+    },
+    {
+        "cableplan_id": "8",
+        "cableplan_name": "DStv Compact Plus",
+        "cableplan_amount": "14250"
+    },
+    {
+        "cableplan_id": "9",
+        "cableplan_name": "DStv Premium",
+        "cableplan_amount": "21000"
+    },
+    {
+        "cableplan_id": "11",
+        "cableplan_name": "Classic - 2,750 Naira - 1 Month",
+        "cableplan_amount": "2750"
+    },
+    {
+        "cableplan_id": "12",
+        "cableplan_name": "Basic - 1,850 Naira - 1 Month",
+        "cableplan_amount": "1850"
+    },
+    {
+        "cableplan_id": "13",
+        "cableplan_name": "Smart - 2,600 Naira - 1 Month",
+        "cableplan_amount": "2600"
+    },
+    {
+        "cableplan_id": "14",
+        "cableplan_name": "Nova - 900 Naira - 1 Month",
+        "cableplan_amount": "900"
+    },
+    {
+        "cableplan_id": "15",
+        "cableplan_name": "Super - 4,900 Naira - 1 Month",
+        "cableplan_amount": "4900"
+    },
+    {
+        "cableplan_id": "16",
+        "cableplan_name": "GOtv Jinja",
+        "cableplan_amount": "1900"
+    },
+    {
+        "cableplan_id": "17",
+        "cableplan_name": "GOtv Jolli",
+        "cableplan_amount": "2800"
+    },
+    {
+        "cableplan_id": "19",
+        "cableplan_name": "DStv Confam",
+        "cableplan_amount": "5300"
+    },
+    {
+        "cableplan_id": "20",
+        "cableplan_name": "DStv Padi",
+        "cableplan_amount": "2150"
+    },
+    {
+        "cableplan_id": "23",
+        "cableplan_name": "DStv Asia",
+        "cableplan_amount": "7100"
+    },
+    {
+        "cableplan_id": "24",
+        "cableplan_name": "DStv Premium French",
+        "cableplan_amount": "29300"
+    },
+    {
+        "cableplan_id": "25",
+        "cableplan_name": "DStv Premium Asia",
+        "cableplan_amount": "23500"
+    },
+    {
+        "cableplan_id": "26",
+        "cableplan_name": "DStv Confam + ExtraView",
+        "cableplan_amount": "8200"
+    },
+    {
+        "cableplan_id": "27",
+        "cableplan_name": "DStv Yanga + ExtraView",
+        "cableplan_amount": "5850"
+    },
+    {
+        "cableplan_id": "28",
+        "cableplan_name": "DStv Padi + ExtraView",
+        "cableplan_amount": "5050"
+    },
+    {
+        "cableplan_id": "29",
+        "cableplan_name": "DStv Compact + Extra View",
+        "cableplan_amount": "11900"
+    },
+    {
+        "cableplan_id": "30",
+        "cableplan_name": "DStv Premium + Extra View",
+        "cableplan_amount": "23900"
+    },
+    {
+        "cableplan_id": "31",
+        "cableplan_name": "DStv Compact Plus - Extra View",
+        "cableplan_amount": "17150"
+    },
+    {
+        "cableplan_id": "32",
+        "cableplan_name": "DStv HDPVR Access Service",
+        "cableplan_amount": "2900"
+    },
+    {
+        "cableplan_id": "33",
+        "cableplan_name": "ExtraView Access",
+        "cableplan_amount": "2900"
+    },
+    {
+        "cableplan_id": "34",
+        "cableplan_name": "GOtv Smallie - Monthly",
+        "cableplan_amount": "900"
+    },
+    {
+        "cableplan_id": "35",
+        "cableplan_name": "GOtv Smallie - Quarterly",
+        "cableplan_amount": "2400"
+    },
+    {
+        "cableplan_id": "36",
+        "cableplan_name": "GOtv Smallie - Yearly",
+        "cableplan_amount": "7000"
+    },
+    {
+        "cableplan_id": "37",
+        "cableplan_name": "Nova - 300 Naira - 1 Week",
+        "cableplan_amount": "300"
+    },
+    {
+        "cableplan_id": "38",
+        "cableplan_name": "Basic - 600 Naira - 1 Week",
+        "cableplan_amount": "600"
+    },
+    {
+        "cableplan_id": "39",
+        "cableplan_name": "Smart - 700 Naira - 1 Week",
+        "cableplan_amount": "700"
+    },
+    {
+        "cableplan_id": "40",
+        "cableplan_name": "Classic - 1200 Naira - 1 Week",
+        "cableplan_amount": "1200"
+    },
+    {
+        "cableplan_id": "41",
+        "cableplan_name": "Super - 1,500 Naira - 1 Week",
+        "cableplan_amount": "1500"
+    },
+    {
+        "cableplan_id": "42",
+        "cableplan_name": "Nova - 90 Naira - 1 Day",
+        "cableplan_amount": "90"
+    },
+    {
+        "cableplan_id": "43",
+        "cableplan_name": "Basic - 160 Naira - 1 Day",
+        "cableplan_amount": "160"
+    },
+    {
+        "cableplan_id": "44",
+        "cableplan_name": "Smart - 200 Naira - 1 Day",
+        "cableplan_amount": "200"
+    },
+    {
+        "cableplan_id": "45",
+        "cableplan_name": "Classic - 320 Naira - 1 Day",
+        "cableplan_amount": "320"
+    },
+    {
+        "cableplan_id": "46",
+        "cableplan_name": "Super - 400 Naira - 1 Day",
+        "cableplan_amount": "400"
+    },
+    {
+        "cableplan_id": "47",
+        "cableplan_name": "Gotv Supa",
+        "cableplan_amount": "5500"
+    }
+]
